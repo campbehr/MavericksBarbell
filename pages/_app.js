8651e860e22e92b9d992881d0eee5c528ed923c5
Hunter Campbell <huntercampbell@Hunters-Air.fios-router.home>
2021-10-25 21:43:42 -0400

    fixing keys for G Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -26,19 +26,17 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Script
-				strategy="lazyOnload"
-				src={`https://www.googletagmanager.com/gtag/js?id=G-${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
 			/>
 
-			<Script strategy="lazyOnload">
+			<Script strategy="afterInteractive">
 				{`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config','${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-		  page_path: window.location.pathname,
-	  });
-  `}
+				window.dataLayer = window.dataLayer || [];
+  				function gtag(){dataLayer.push(arguments);}
+  				gtag('js', new Date());
+  				gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+  				`}
 			</Script>
 			<ToastProvider
 				components={{ Toast: CustomToast }}
